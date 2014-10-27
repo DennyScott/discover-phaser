a00d7ddd3da15e108385415ab6fde9ee573426dd
Denny Scott <dennyscott301@gmail.com>
2014-10-26 19:43:37 -0500

    Added Walls, collisions, and resets, completed 3.3

--- app/main.js
+++ app/main.js
@@ -9,6 +9,8 @@ var mainState = {
 		// That's where we load the game's assets
 
 		game.load.image('player', 'assets/player.png');
+		game.load.image('wallV', 'assets/wallVertical.png');
+		game.load.image('wallH', 'assets/wallHorizontal.png');
 
 	},
 	create: function() {
@@ -29,13 +31,21 @@ var mainState = {
 		this.player.body.gravity.y = 500;
 
 		this.cursor = game.input.keyboard.createCursorKeys();
+		this.createWorld();
 	},
 
 	update: function() {
 		// This function is called 60 times per second 
 		// It contains the game's logic
+		
+		//Tell Phaser that the player and the walls should collide
+		game.physics.arcade.collide(this.player, this.walls);
 
 		this.movePlayer();
+
+		if(!this.player.inWorld) {
+			this.playerDie();
+		}
 	},
 
 	movePlayer: function() {
@@ -63,6 +73,36 @@ var mainState = {
 			this.player.body.velocity.y = -320;
 		}
 	},
+
+	createWorld: function() {
+		//Create Our wall group with Arcade Physics
+		this.walls = game.add.group();
+		this.walls.enableBody = true;
+
+		// Create the 10 walls
+		game.add.sprite(0,0, 'wallV', 0, this.walls); //Left
+		game.add.sprite(480, 0, 'wallV', 0, this.walls); //Right
+
+		game.add.sprite(0,0, 'wallH', 0, this.walls); //Top Left
+		game.add.sprite(300, 0, 'wallH', 0, this.walls); //Top Right
+		game.add.sprite(0, 320, 'wallH', 0, this.walls); //Bottom Left
+		game.add.sprite(300, 320, 'wallH', 0, this.walls); //Bottom Right
+
+		game.add.sprite(-100, 160, 'wallH', 0, this.walls); //Middle left
+		game.add.sprite(400, 160, 'wallH', 0, this.walls); //Middle right
+
+		var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+		middleTop.scale.setTo(1.5, 1);
+		var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+		middleBottom.scale.setTo(1.5, 1);
+
+		// Set all the walls to be immovable
+		this.walls.setAll('body.immovable', true);
+	},
+
+	playerDie: function() {
+		game.state.start('main');
+	}
 
 };
 
